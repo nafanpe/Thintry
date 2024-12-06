dab75f15b48979f5244054b1f4cfe303f3a4ff79
mrsajadpp <mrsajadpp@gmail.com>
2024-12-07 00:00:28 +0530

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/fav/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/fav/favicon.svg" />
+        <link rel="shortcut icon" href="/fav/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/fav/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/fav/site.webmanifest" />
+        <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css'></link>
+      </head>
       <body>
         <div className='homepage-master-container'>
           <div className='homepage-container'>
