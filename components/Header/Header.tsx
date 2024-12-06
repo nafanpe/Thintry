5c61dd940ab98f33f4973abcb8d74d6f9925ce6a
mrsajadpp <mrsajadpp@gmail.com>
2024-12-07 00:08:37 +0530

    bug fixed

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,45 +1,48 @@
 import '@/components/Header/Header.css';
+import Link from 'next/link';
 
-function Header(){
-    return(
+function Header() {
+    return (
         <div className='left-bar'>
-                <div className='logo'>
+            <div className='logo'>
+                <Link href="/">
                     <img src="/img/green-fire-logo-black-text.png" alt="Thintry.logo" />
-                </div>
-                <nav>
-                    <ul>
-                        <div className='option-ul'>
-                            <img src="/img/icons/home.svg" /><li><a href="/"> Read </a></li>
-                        </div>
+                </Link>
+            </div>
+            <nav>
+                <ul>
+                    <div className='option-ul'>
+                        <img src="/img/icons/home.svg" /><li><a href="/"> Read </a></li>
+                    </div>
 
-                        <div className='option-ul'>
-                            <img src="/img/icons/rocket.svg" /><li><a href="/articles">Spotlight</a></li>
-                        </div>
-                        
-                        <div className='option-ul'>
-                            <img src="/img/icons/search.svg" /><li><a href="/spotlight">Search</a></li>
-                        </div>
+                    <div className='option-ul'>
+                        <img src="/img/icons/rocket.svg" /><li><a href="/articles">Spotlight</a></li>
+                    </div>
 
-                        {/* <div className='option-ul'>
+                    <div className='option-ul'>
+                        <img src="/img/icons/search.svg" /><li><a href="/spotlight">Search</a></li>
+                    </div>
+
+                    {/* <div className='option-ul'>
                             <img src="https://unpkg.com/@mynaui/icons/icons/home.svg" /><li><a href="/articles"> Articles </a></li>
                         </div>
 
                         <div className='option-ul'>
                             <img src="https://unpkg.com/@mynaui/icons/icons/home.svg" /><li><a href="/jobs"> Jobs </a></li>
                         </div> */}
-                    </ul>
-                </nav>
-                <button className='quick-actions'>
-                    <img src="/img/icons/pencil.svg" alt="Img" />
-                    New Article
-                </button>
-                {/* <button className='create-article'>
+                </ul>
+            </nav>
+            <button className='quick-actions'>
+                <img src="/img/icons/pencil.svg" alt="Img" />
+                New Article
+            </button>
+            {/* <button className='create-article'>
                     <img src="/assets/images/handwriting2.jpeg" alt="draw-image" />
                     <h3>Hey, Do you Write?</h3>
                     <p>Now, Publish your Article from your Profile</p>
                     <h4>Create Article <img src="/img/icons/arrow-right.svg" alt="" /></h4>
                 </button> */}
-            </div>
+        </div>
     )
 }
 
